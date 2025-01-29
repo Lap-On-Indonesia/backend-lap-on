46bc4fb9a5d8e9a479b1837d0b7ac8e163daad4f
Pid24 <rofidnasifannafie@gmail.com>
2025-01-29 18:39:07 +0700

    add id in product

--- database/migrations/2024_08_10_164304_create_products_table.php
+++ database/migrations/2024_08_10_164304_create_products_table.php
@@ -12,24 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->string('product_id')->unique()->primary();
+            $table->bigIncrements('id'); // Kolom id sebagai primary key dan auto-increment
+            $table->string('product_id')->unique(); // Kolom product_id yang unik
             $table->string('name_product');
             $table->string('image');
-            $table->foreignId('category_marketplace_id');
-            $table->foreign('category_marketplace_id')->references('id')->on('category_marketplaces')->onDelete('cascade');
+            $table->foreignId('category_marketplace_id')->constrained()->onDelete('cascade');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->integer('stock')->default(0); // Menambahkan field stock
             $table->timestamps();
             $table->softDeletes();
-        });
-
-        // Menambahkan kolom owner_marketplace_id jika belum ada
-        Schema::table('products', function (Blueprint $table) {
-            if (!Schema::hasColumn('products', 'owner_marketplace_id')) {
-                $table->foreignId('owner_marketplace_id')->nullable();
-                $table->foreign('owner_marketplace_id')->references('id')->on('owner_marketplaces')->onDelete('cascade');
-            }
         });
     }
 
@@ -38,13 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            if (Schema::hasColumn('products', 'owner_marketplace_id')) {
-                $table->dropForeign(['owner_marketplace_id']);
-                $table->dropColumn('owner_marketplace_id');
-            }
-        });
-
         Schema::dropIfExists('products');
     }
 };
