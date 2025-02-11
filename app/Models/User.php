c545f498bea5443888949398dc49ee4e00bfed49
reonaldi152 <reonaldi152@gmail.com>
2025-02-11 16:39:21 +0000

    fixing booking, descrease stock when payment

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,4 +72,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(OwnerMarketplace::class);
     }
+
+    public function venues()
+{
+    return $this->hasMany(Venue::class, 'owner_id');
+}
 }
